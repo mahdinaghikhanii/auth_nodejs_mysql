61c3b9302cf2e7dc5db9a18aba7aa9cb7f21d047
Mahdinaghikhani <mahdinkh7@gmail.com>
2023-02-18 20:19:57 +0330

    craert userRouter and connction to my Sql

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,19 +4,22 @@ const sabzLearnDB = require('../db/sabzlearnDB');
 
 
 userRoutes.post('/', (req, res) => {
+    let date = new Date().toLocaleDateString('fa-IR');
+    const body = req.body;
     sabzLearnDB.connect((err)=> {
         if(err){
             console.log("You have Error", err);
         }else{
             console.log("Connect to sablearnDB succesfulyy");
-            let newUserInsertQuery = 'insert into users values (2,"elina","mohammadi","mahdi1379","1401/11/27","elina")';
+            let newUserInsertQuery = `insert into users values (4 ${body.firstname},${body.lastname}, ${body.username}, ${body.password},${date})`;
             sabzLearnDB.query(newUserInsertQuery , (error , result)=> {
                 if(error){
                     console.log("Insert user fail",error);
+                    res.send(null);
                 }else{
                     console.log("One User Inserted");
+                    res.send(true);
                 }
-    
             })
         }
     }) 
